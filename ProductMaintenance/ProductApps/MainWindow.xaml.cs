830bc93aa6050219fcacad5bf61f5ea3dcd72d08
001186052 <Farzaneh.NEMATPOUR@student.tafesa.edu.au>
2024-04-01 23:25:02 +1030

    Added the delivery charge $25

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -34,6 +34,8 @@ namespace ProductApps
                 cProduct = new Product(Convert.ToDecimal(priceTextBox.Text), Convert.ToInt16(quantityTextBox.Text));
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
+                decimal totalCharge = cProduct.TotalPayment + 25.00m;
+                totalChargeTextBlock.Text = totalCharge.ToString();
             }
             catch (FormatException)
             {
@@ -47,6 +49,8 @@ namespace ProductApps
             priceTextBox.Text = "";
             quantityTextBox.Text = "";
             totalPaymentTextBlock.Text = "";
+            totalChargeTextBlock.Text = "";
+
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
